9819e5cb062d6591ce107b369e9d2df1e8e31231
Nicola Molinari <nicola.molinari@commercetools.de>
2013-11-22 15:08:51 +0100

    Update url path to play 1.2.5

--- play125.rb
+++ play125.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Play125 < Formula
   homepage 'http://www.playframework.org/'
-  url 'https://github.com/playframework/play1/archive/1.2.5.tar.gz'
-  sha1 '3a7299d8a9d65cb77295ed36605f5ebf726f6517'
+  url 'http://download.playframework.org/releases/play-1.2.5.zip'
+  sha1 '31d204bb105f67c5e418fad073e818a4'
 
   def install
     rm_rf 'python' # we don't need the bundled Python for windows
